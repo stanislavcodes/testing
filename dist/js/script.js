5e37642e6ad8aea39d3ccb236e1eb19bc6f5d1ab
stanislavcodes <korchevsky2004@gmail.com>
2020-06-24 12:25:16 +0300

    test

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,10 +3,14 @@ const questions = document.querySelectorAll(".question");
 const questionBoxes = document.querySelectorAll(".question-box");
 const answers = document.querySelectorAll(".answer");
 const answerTexts = document.querySelectorAll(".answer__text");
+
 // event listenetrs
+
 for (let i = 0; i < questions.length; i++) {
   questions[i].addEventListener("click", check(i));
 }
+//
+
 function calcAnswerHeight(par) {
   let d = par;
   let questionHeight = window.getComputedStyle(questions[d]).height;
@@ -15,40 +19,46 @@ function calcAnswerHeight(par) {
   let answerPaddingBottom = window.getComputedStyle(answers[d]).paddingBottom;
   questionBoxes[d].style.height = `calc(${questionHeight} + ${answerHeight})`;
 }
+
 //questions and answers logic
 (function (a) {
   // open first answer when page is loaded'
-  for(i = 0; i < questionBoxes.length; i++) {
-    questionBoxes[i].classList.add("closed");
-  }
-  questionBoxes[a].classList.remove("closed");
   questionBoxes[a].classList.add("open");
   questions[a].classList.add("open");
+  answers[a].classList.add("open");
   // // calculating height
   calcAnswerHeight(a);
 })(0);
+
 //returns elements to their's default values
 function defaultState() {
   for (i = 0; i < questions.length; i++) {
-    questionBoxes[i].classList.add("closed");
-    questionBoxes[i].classList.remove("open");
+    answers[i].classList.add("closed");
+    answers[i].classList.remove("open");
+
     let questionHeight = window.getComputedStyle(questions[i]).height;
     questionBoxes[i].style.height = `${questionHeight}`;
+    console.log(questionHeight);
+
     questions[i].classList.remove("open");
-    questionBoxes[i].classList.remove("open");
+    answers[i].classList.remove("open");
   }
 }
+
 function fn(a) {
   defaultState();
-  questionBoxes[a].classList.remove("closed");
+  answers[a].classList.remove("closed");
+
   questionBoxes[a].classList.add("open");
   questions[a].classList.add("open");
+  answers[a].classList.add("open");
+  // // calculating height
   calcAnswerHeight(a);
 }
 // check if the answer is closed or opened
 function check(b) {
   questions[b].addEventListener("click", function () {
-    if (questionBoxes[b].classList.contains("closed")) {
+    if (answers[b].classList.contains("closed")) {
       fn(b);
     } else {
       defaultState();
